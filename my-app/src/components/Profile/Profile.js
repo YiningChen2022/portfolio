9315f102070caca244b1f0840f32432b5c28a0e0
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-03-09 09:51:34 -0500

    add info
    add info

--- my-app/src/components/Profile/Profile.js
+++ my-app/src/components/Profile/Profile.js
@@ -2,20 +2,60 @@ import React from "react";
 import { Typography } from "@material-ui/core";
 import "./Profile.css";
 import myImage from "../../assets/images/profile.jpg";
-import CustomTimeline from "../Timeline/CustomTimeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import CustomTimeline, {
+  CustomTimelineSeparator,
+} from "../Timeline/CustomTimeline";
+import resumeData from "../../utils/resumeData";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+
+const CustomTimelineItem = ({ title, text, link }) => (
+  <TimelineItem>
+    <CustomTimelineSeparator />
+    <TimelineContent>
+      {link ? (
+        <Typography>
+          <span>{title}:</span>
+          <a href={link} target="_blank">
+            {text}
+          </a>
+        </Typography>
+      ) : (
+        <Typography>
+          <span>{title}:</span>
+          {text}
+        </Typography>
+      )}
+    </TimelineContent>
+  </TimelineItem>
+);
 
 const Profile = () => {
   return (
     <div className="profile container_shadow">
       <div className="profile_name">
-        <Typography className="name">Yining</Typography>
-        <Typography className="title">yining title</Typography>
+        <Typography className="name">{resumeData.name}</Typography>
+        <Typography className="title">{resumeData.title}</Typography>
       </div>
       <figure className="profile_image">
         <img src={myImage} alt="" />
       </figure>
       <div className="profile_information">
-        <CustomTimeline/>
+        <CustomTimeline icon={<PersonOutlineOutlinedIcon />}>
+          <CustomTimelineItem
+            title="Name"
+            text={resumeData.name}
+          ></CustomTimelineItem>
+          <CustomTimelineItem
+            title="Email"
+            text={resumeData.email}
+          ></CustomTimelineItem>
+          <CustomTimelineItem
+            title="Phone"
+            text={resumeData.phone}
+          ></CustomTimelineItem>
+        </CustomTimeline>
         <br />
         <button>my button</button>
       </div>
---
+++ my-app/src/utils/resumeData.js
@@ -0,0 +1,22 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+export default {
+  name: "Yining Chen",
+  title: "Full Stack Developer",
+  email: "chen.yinin@northeastern.edu",
+
+  phone: "732 668 0885",
+  socials: {
+    linkedin: {
+      link: "http://linkedin.com/in/yiningchen2018",
+      text: "View My Linkedin",
+      icon: <LinkedInIcon />,
+    },
+    github: {
+      link: "https://github.com/YiningChen2022",
+      text: "View My GitHub",
+      icon: <GitHubIcon />,
+    },
+  },
+};
