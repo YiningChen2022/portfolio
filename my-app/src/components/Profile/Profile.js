a62464c470d2cf426a878aa8199c71cb13ddacff
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-03-08 23:43:35 -0500

    upload image

--- my-app/src/components/Profile/Profile.js
+++ my-app/src/components/Profile/Profile.js
@@ -1,7 +1,25 @@
 import React from "react";
-const Profile = () => {
-  return <div className="profile container_shadow">
+import { Typography } from "@material-ui/core";
+import "./Profile.css"
+import myImage from '../../assets/images/profile.jpg'
 
-   </div>;
+
+const Profile = () => {
+  return (
+    <div className="profile container_shadow">
+      <div className="profile_name">
+        <Typography className="name">Yining</Typography>
+        <Typography className="title">yining title</Typography>
+      </div>
+      <figure className="profile_image">
+        <img src={myImage} alt="" />
+      </figure>
+      <div className="profile_information">
+        insert Timeline
+        <br />
+        <button>my button</button>
+      </div>
+    </div>
+  );
 };
 export default Profile;
