fac81b3e04b7a80872abf388f8fb85b239de12b6
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-03-09 20:59:52 -0500

    header

--- my-app/src/pages/Resume/Resume.js
+++ my-app/src/pages/Resume/Resume.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "./Resume.css";
+import { Grid } from "@material-ui/core";
 const Resume = () => {
-  return <div>this is a resume component </div>;
+  return;
+  <>
+    {/*About me*/}
+    <Grid container className=""></Grid>;{/*Education and experiences*/}
+    <Grid container className=""></Grid>;{/*Services*/}
+    <Grid container className=""></Grid>;{/*Skills*/}
+    <Grid container className=""></Grid>;{/*Contact*/}
+    <Grid container className=""></Grid>;
+  </>;
 };
 export default Resume;
